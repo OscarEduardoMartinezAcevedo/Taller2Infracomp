403218bd3a0a0875d10734b446d8dfaca9faa4ce
Alejandra Sanabria <128005730+Aleja-xd@users.noreply.github.com>
2025-01-31 11:48:30 -0500

    Update Main.java

--- Taller2/src/Parqueadero/Main.java
+++ Taller2/src/Parqueadero/Main.java
@@ -1,4 +1,5 @@
 package Parqueadero;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,19 +8,31 @@ public class Main {
         Estacionamiento parqueaderoE2 = new Estacionamiento(capacidad);
         System.out.println("Inicializacion del parqueadero");
         
-        /**
-         * Espera activa, el booleano en @Auto determina que for usar, en este caso el siguiente
-         */
-        for (int i = 0; i < numAutos; i++) {
-            new Auto(parqueaderoE2, true,i).start();
-        }
-        
-        
-        /**
-         * Espera semi-pasiva, el booleano en @Auto determina que for usar, en este caso el siguiente
-         */
-        for (int i = 0; i < numAutos; i++) {
-            new Auto(parqueaderoE2, false,i).start();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Seleccione el tipo de espera:");
+        System.out.println("1 - Espera Activa");
+        System.out.println("2 - Espera Semi-Pasiva");
+        int opcion = scanner.nextInt();
+        scanner.close();
+
+        switch (opcion) {
+            case 1:
+                System.out.println("Ejecutando con espera activa...");
+                for (int i = 0; i < numAutos; i++) {
+                    new Auto(parqueaderoE2, true, i).start();
+                }
+                break;
+
+            case 2:
+                System.out.println("Ejecutando con espera semi-pasiva...");
+                for (int i = 0; i < numAutos; i++) {
+                    new Auto(parqueaderoE2, false, i).start();
+                }
+                break;
+
+            default:
+                System.out.println("Opción no válida. Saliendo...");
+                break;
         }
     }
 }
